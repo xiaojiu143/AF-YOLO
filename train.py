6e498711b7e33fac3d66b38ac624ccfe892b7a00
xiaojiu143 <107339025+xiaojiu143@users.noreply.github.com>
2025-07-19 21:21:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     model = YOLO("AF-YOLO")
-    model.train(data="your.ymal", epochs=500, batch=8, imgsz=640, patience=50, deterministic=False)
+    model.train(data="your.yaml", epochs=500, batch=8, imgsz=640, patience=50, deterministic=False)
